a59456490648933e718fd8e9773ab6e556350fe9
Grace Milton <gmmilton10@gmail.com>
2019-05-01 13:50:07 -0400

    testing ajax

--- app.py
+++ app.py
@@ -6,3 +6,14 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 #    return "Hello World!"
+
+#rendering the HTML page which has the button
+@app.route('/json')
+def json():
+    return render_template('json.html')
+
+#background process happening without any refreshing
+@app.route('/background_process_test')
+def background_process_test():
+    print "Hello"
+    return "nothing"
