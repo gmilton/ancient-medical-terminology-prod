15688e3f43214d593ef111438c6b760fe1dffe0d
Grace Milton <gmmilton10@gmail.com>
2019-05-04 13:02:44 -0400

    add words

---
+++ words.py
@@ -0,0 +1,39 @@
+import csv
+
+FILENAME = 'word_lists.csv'
+
+class Word(object): 
+    def __init__(self, word): 
+        self.term = word[0].replace("-", "").replace(" ", "") 
+        self.definition = word[1] 
+        self.greek_latin = word[2] 
+        self.pos = word[3] 
+ 
+class Node(object): 
+    def __init__(self, word): 
+        self.word = word 
+        self.children = [] 
+ 
+    def __str__(self, level=0): 
+        ret = "\t"*level+repr(self.word.term)+"\n" 
+        for child in self.children: 
+            ret += child.__str__(level+1) 
+        return ret 
+ 
+    def __repr__(self): 
+        return '<tree node representation>' 
+ 
+class Tree(object): 
+    def __init__(self, root): 
+        self.root = root 
+
+def load_words():
+    word_list = []
+
+    with open(FILENAME, newline='') as csvfile:
+        wlreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for index, row in enumerate(wlreader):
+            if index: # ignore first line with template
+                word_list.append(Word(row))
+
+    return word_list
