8862e8461acaf8f1b88949c76187bbdadbd806a7
Grace Milton <gmmilton10@gmail.com>
2019-05-04 13:00:57 -0400

    progress

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from words import load_words, Word, Node, Tree
 app = Flask(__name__)
 
 TERMS, DEFS = "terms", "definitions"
@@ -6,6 +7,17 @@ AVAILABLE_COMMANDS = {
     'Terms': TERMS,
     'Definitions': DEFS
 }
+WORD_LIST = load_words()
+
+def search_terms(word_list, search):
+    new_word_list = []
+
+    for word in word_list:
+        if search.lower() in word.term.lower():
+            print(search.lower(), word.term.lower())
+            new_word_list.append(word)
+
+    return sorted(new_word_list, key=lambda w: w.term)
 
 @app.route('/')
 def hello():
@@ -18,14 +30,21 @@ def hello():
 
 @app.route('/<cmd>')
 def command(cmd=None):
-    if cmd == TERMS:
+    cmd = cmd.split(";")
+    if cmd[0] == TERMS:
        camera_command = "X"
        f = open("word_lists.csv")
        line = f.readline()
-       response = "Searching {}: {}".format(cmd.capitalize(), line)
+       if cmd[1]:
+           search = cmd[1]
+           CONTAINS = cmd[1]
+       else:
+           search = "NULL"
+       terms_list = search_terms(WORD_LIST, search)
+       response = "Searching {} for {}: {}".format(cmd[0].capitalize(), search, terms_list)
     else:
-        camera_command = cmd[0].upper()
-        response = "Searching {}".format(cmd.capitalize())
+        camera_command = cmd[0][0].upper()
+        response = "Searching {}".format(cmd[0].capitalize())
 
     # ser.write(camera_command)
     return response, 200, {'Content-Type': 'text/plain'}
